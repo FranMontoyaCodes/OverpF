b6b960d939a1aaa59fb2e3c8f239e4effa55b2d1
FranMontoyaCodes <f.a.montoya.2020@gmail.com>
2023-03-24 10:20:40 -0600

    repooo

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret
 SECRET_KEY = 'django-insecure-ij+0*v%e&6n+-oy2jp-o&wfqpo9#0eo+gx_5*)n57kfes&!a0v'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-#API apps
+#API apps 
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -61,6 +61,7 @@ SOCIALACCOUNT_PROVIDERS = { # add googel as a social account provider
 }
 
 MIDDLEWARE = [
+    #+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
